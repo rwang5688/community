0265b6427bdebad1f6194f1a44d1c47692b67cda
Robert Wang <rwang5688@outlook.com>
2021-03-12 01:30:38 -0800

    rename community-sftp shell scripts

--- resources/create-community-sftp.sh
+++ resources/create-community-sftp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name sftp-community \
+aws cloudformation create-stack --stack-name community-sftp \
 --template-body file://sftp-server.yaml \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameters \
@@ -8,4 +8,4 @@ ParameterKey=PublicSubnet1,ParameterValue=subnet-4256fb29 \
 ParameterKey=PublicSubnet2,ParameterValue=subnet-d0a2b9aa \
 ParameterKey=BucketStack,ParameterValue=community-bucket \
 ParameterKey=CustomHostname,ParameterValue=sftp-community.octank.edu.rwang5688.com \
-ParameterKey=HostedZoneId,ParameterValue=Z05028892SWHRH407JS0E
+ParameterKey=HostedZone,ParameterValue=Z05028892SWHRH407JS0E
