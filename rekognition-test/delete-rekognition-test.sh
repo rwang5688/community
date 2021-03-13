d1f61182adc28984adc9d0465de6f65813ade8bd
Robert Wang <rwang5688@outlook.com>
2021-03-13 02:22:43 -0800

    add scripts for cleaning out data buckets

--- rekognition-test/delete-rekognition-test.sh
+++ rekognition-test/delete-rekognition-test.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-aws s3 rm s3://rekognition-test-data-bucket --recursive
 aws cloudformation delete-stack --stack-name rekognition-test
---
+++ resources/clean-community-bucket.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws s3 rm s3://community.octank.edu.rwang5688.com --recursive
