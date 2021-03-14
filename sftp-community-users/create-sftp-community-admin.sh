44ee1aa627d1425392be7d5003663605049124ab
Robert Wang <rwang5688@outlook.com>
2021-03-14 12:57:13 -0700

    rename sftp community users scripts and templates

--- sftp-community-users/create-sftp-community-admin.sh
+++ sftp-community-users/create-sftp-community-admin.sh
@@ -2,7 +2,7 @@
 echo "Create SFTP-Community Admin=$1, Parameters File=$1.json"
 
 aws cloudformation create-stack --stack-name sftp-community-admin-$1 \
---template-body file://sftp-admin.yaml \
+--template-body file://sftp-community-admin.yaml \
 --parameters file://$1.json
 
 echo "Done"
