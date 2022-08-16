aec41f83efae89753a903d67c91f30cbeea92a78
Robert Wang <rwang5688@outlook.com>
2022-08-16 04:27:42 +0000

    delete hard coded shell scripts

--- src/community-bucket/deploy-guided-community-bucket-wangrob-sandbox-01.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Deploy community-bucket stack (including publishContent function) in wangrob-sandbox-01."
-
-sam deploy --guided --stack-name community-bucket \
---template-file community-bucket.yaml \
---profile wangrob-sandbox-01 \
---region us-east-1
-
-echo "Done"
